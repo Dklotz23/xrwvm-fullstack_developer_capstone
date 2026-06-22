39e167064d002aa18b73190947ea86155582a44c
Dklotz23 <devinmklotz@gmail.com>
2026-06-22 16:51:19 -0400

    corrected last  linting errors in python files

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -41,7 +41,7 @@ CSRF_TRUSTED_ORIGINS = [
         'proxy.cognitiveclass.ai'
     ),
     (
-        'https://devinmklotz-8000.theiadockernext-1-labs-prod-theiak8s-
+        'https://devinmklotz-8000.theiadockernext-1-labs-prod-theiak8s-'
         '4-tor01.proxy.cognitiveclass.ai'
     )
 ]
