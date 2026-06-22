30f4d088e3f4ba4a4dbc1bd66aeab16b135a6a6f
Dklotz23 <devinmklotz@gmail.com>
2026-06-22 16:17:42 -0400

    linted python and js files

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,22 +2,23 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-# Register your models here.
-
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 1
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('car_make', 'name', 'type', 'year')  
+    list_display = ('car_make', 'name', 'type', 'year')
 
-admin.site.register(CarModel)      
+
+admin.site.register(CarModel)
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     inlines = [CarModelInline]
+
 
 # Register models here
 admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,5 @@
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -14,7 +13,7 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -28,11 +27,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2026,
-        validators=[
-            MaxValueValidator(2026),
-            MinValueValidator(2010)
-        ])
+                               validators=[
+                                   MaxValueValidator(2026),
+                                   MinValueValidator(2010)
+                                ])
 
     def __str__(self):
         return self.name
-
